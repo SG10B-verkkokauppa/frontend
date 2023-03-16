cd2b2cd72eddda09859c4d47bcd34140472a781e
Veetny <veetinym@gmail.com>
2023-03-16 11:48:41 +0200

    Alku etusivu

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+
+    <div className="Container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+            <label class="logo"><a href="#">Verkkokauppa</a></label>
+            <button type="button" id="hamburger" onclick="hideNav()"><i class="fa-solid fa-bars fa-xl"></i></button>
+            <div id="navi" class="hide"></div>
+        <ul>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Retro</a></li>
+            <li><a href="#">Collector items</a></li>
+        </ul>
+        </nav>
       </header>
+      
     </div>
   );
 }
