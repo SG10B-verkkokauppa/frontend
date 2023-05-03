267b2b505d068170d041c7777a1779e4f2ba5d99
Veetny <veetinym@gmail.com>
2023-05-03 21:47:57 +0300

    Ens

--- src/pages/Konsolit.js
+++ src/pages/Konsolit.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import product1 from "./images/ps5.png";
 
 export default function Konsolit(){
     return (
     <div className="Container">
+      <div className="Product">
+        <img src={product1} className="productimg"></img>
+        <p>Name:Playstation 5</p><br></br>
+        <p>Price:600€</p>
+      </div>
       
-      <p>Muutama konsoli</p>
     </div>
     );
 }
