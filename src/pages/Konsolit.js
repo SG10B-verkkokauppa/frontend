4297b1f4ae9b5525ca18de8d2614bb46622d220d
Veetny <veetinym@gmail.com>
2023-05-03 23:16:12 +0300

    psuh

--- src/pages/Konsolit.js
+++ src/pages/Konsolit.js
@@ -1,16 +1,75 @@
 import React from "react"
 import { Link } from "react-router-dom";
-import product1 from "./images/ps5.png";
+import product5 from "./images/ps5.png";
+import product6 from "./images/switch.png";
+import product7 from "./images/xboxseriesX.png"
 
 export default function Konsolit(){
-    return (
+  return (
+    <>
+    <label className="productlabel">Konsolit</label>
     <div className="Container">
-      <div className="Product">
-        <img src={product1} className="productimg"></img>
-        <p>Name:Playstation 5</p><br></br>
-        <p>Price:600€</p>
+      <div class="row product">
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product6} className="productimg"></img>
+            <label>Name:Switch</label>
+            <p>Price:300€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product7} className="productimg"></img>
+            <label>Name:XboxseriesX</label>
+            <p>Price:520€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div className="Product border">
+            <img src={product5} className="productimg"></img>
+            <label>Name:Playstation 5</label>
+            <p>Price:600€</p>
+          </div>
+        </div>
+
       </div>
       
     </div>
+    </>
     );
 }
--- src/pages/Oheistuotteet.js
+++ src/pages/Oheistuotteet.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom";
-import product1 from "./images/amiibolink.png"
+import product3 from "./images/amiibolink.png"
 
 export default function Oheistuotteet(){
     return (
     <div className="Container">
       
       <div className="Product">
-        <img src={product1} className="productimg"></img>
+        <img src={product3} className="productimg"></img>
         <p>Name:Playstation 5</p><br></br>
         <p>Price:600€</p>
       </div>
