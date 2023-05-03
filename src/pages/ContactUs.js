938184f71f29ea5cae9c97e9b62521854cd78477
Tompetin <tommisalo.jokeri@elisanet.fi>
2023-05-03 23:16:11 +0300

    about ja contact us sisältöä

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 export default function ContactUs(){
     return (
     <div className="Container">
-      
-      <p>Here is our contact information</p>
+      <h1>Tässä on meidän yhteystietomme:</h1>
+      <label>Sähköposti: customersupport@playlist.com</label><br></br>
+      <label>Puhelin (arkisin 12-12.15): +358 40 567890123</label>
     </div>
     );
 }
