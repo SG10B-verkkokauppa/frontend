f78da18f29b318145828cb871025689beb4118a3
oggysi <oskari.ontero@gmail.com>
2023-05-04 00:22:06 +0300

    xxl md

--- src/pages/Oheistuotteet.js
+++ src/pages/Oheistuotteet.js
@@ -12,35 +12,35 @@ export default function Oheistuotteet(){
       <label className="productlabel">Oheistuotteet</label>
       <div className="Container">
         <div class="row product">
-          <div class="col-xxl-3">
+          <div class="col-md-3">
             <div className="Product border">
               <img src={product1} className="productimg"></img>
               <label>Name:Game boy avaimenperä</label>
               <p>Price:7,7€</p>
             </div>
           </div>
-          <div class="col-xxl-3">
+          <div class="col-md-3">
             <div className="Product border">
               <img src={product2} className="productimg"></img>
               <label>Name:Minecraft muki</label>
               <p>Price:9,9€</p>
             </div>
           </div>
-          <div class="col-xxl-3">
+          <div class="col-md-3">
             <div className="Product border">
               <img src={product3} className="productimg"></img>
               <label>Name:Amiibo 8-bit Link</label>
               <p>Price:24,9€</p>
             </div>
           </div>
-          <div class="col-xxl-3">
+          <div class="col-md-3">
             <div className="Product border">
               <img src={product4} className="productimg"></img>
               <label>Name:Amiibo Charizard No. 33 (SSB Collection)</label>
               <p>Price:24,9€</p>
             </div>
           </div>
-          <div class="col-xxl-3">
+          <div class="col-md-3">
             <div className="Product border">
               <img src={product5} className="productimg"></img>
               <label>Name:Amiibo Luigi (Super Mario Series)</label>
